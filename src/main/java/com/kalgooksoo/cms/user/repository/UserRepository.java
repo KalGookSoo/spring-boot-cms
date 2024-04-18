fae2e845f5b489ba539706665804eb7ac9cb92d7
kimdoeyob <ga.miro3721@gmail.com>
2024-04-18 09:34:53 +0900

    test: UserRepositoryTest 추가

--- src/main/java/com/kalgooksoo/cms/user/repository/UserRepository.java
+++ src/main/java/com/kalgooksoo/cms/user/repository/UserRepository.java
@@ -1,10 +1,23 @@
 package com.kalgooksoo.cms.user.repository;
 
 import com.kalgooksoo.cms.user.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.repository.Repository;
 
 import java.util.Optional;
 
-public interface UserRepository extends JpaRepository<User, String> {
+public interface UserRepository extends Repository<User, String> {
+    User save(User user);
+
+    Optional<User> findById(String id);
+
+    Page<User> findAll(Pageable pageable);
+
+    void deleteById(String id);
+
     Optional<User> findByUsername(String username);
+
+    Page<User> searchAll(String keyword, Pageable pageable);
+
 }
