2ae47d1aebf03453be7c7c11435d567cdd718d8f
kimdoyeob <ga.miro3721@gmail.com>
2024-04-21 18:41:24 +0900

    style; @Nonnull을 @NonNull로 변경 javax 패키지 애노테이션을 스프링 애노테이션으로 변경

--- src/main/java/com/kalgooksoo/cms/user/repository/UserRepository.java
+++ src/main/java/com/kalgooksoo/cms/user/repository/UserRepository.java
@@ -4,20 +4,18 @@ import com.kalgooksoo.cms.user.entity.User;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.Repository;
+import org.springframework.lang.NonNull;
 
 import java.util.Optional;
 
 public interface UserRepository extends Repository<User, String> {
-    User save(User user);
+    User save(@NonNull User user);
 
-    Optional<User> findById(String id);
+    Optional<User> findById(@NonNull String id);
 
-    Page<User> findAll(Pageable pageable);
+    Page<User> findAll(@NonNull Pageable pageable);
 
-    void deleteById(String id);
+    void deleteById(@NonNull String id);
 
-    Optional<User> findByUsername(String username);
-
-    Page<User> searchAll(String keyword, Pageable pageable);
-
+    Optional<User> findByUsername(@NonNull String username);
 }
