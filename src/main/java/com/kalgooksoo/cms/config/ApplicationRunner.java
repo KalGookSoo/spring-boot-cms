37f034ad7cfab8d1f04d27de8f9faab685bba951
kimdoeyob <ga.miro3721@gmail.com>
2024-04-25 13:17:52 +0900

    feat: Remember me 초안 작성

--- src/main/java/com/kalgooksoo/cms/config/ApplicationRunner.java
+++ src/main/java/com/kalgooksoo/cms/config/ApplicationRunner.java
@@ -5,33 +5,35 @@ import com.kalgooksoo.cms.user.service.UserService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.context.event.ApplicationStartedEvent;
+import org.springframework.context.ApplicationListener;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Lazy;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.lang.NonNull;
 
 @Configuration
-public class ApplicationRunner implements CommandLineRunner {
+public class ApplicationRunner implements CommandLineRunner, ApplicationListener<ApplicationStartedEvent> {
 
     private final Logger logger = LoggerFactory.getLogger(getClass());
 
     private final UserService userService;
 
-    public ApplicationRunner(@Lazy UserService userService) {
+    public ApplicationRunner(UserService userService) {
         this.userService = userService;
     }
 
     @Override
+    public void onApplicationEvent(@NonNull ApplicationStartedEvent event) {
+        logger.info("Application started");
+    }
+
+    @Override
     public void run(String... args) {
-        CreateUserCommand createAdminCommand = new CreateUserCommand(
-                "admin",
-                "12341234",
-                "관리자",
-                "ga.miro3721",
-                "gmail.com",
-                "010",
-                "1234",
-                "5678"
-        );
+        createAdmin();
+        createUser();
+    }
+
+    private void createUser() {
         CreateUserCommand createUserCommand = new CreateUserCommand(
                 "tester",
                 "12341234",
@@ -43,16 +45,28 @@ public class ApplicationRunner implements CommandLineRunner {
                 "5678"
         );
         try {
-            userService.createAdmin(createAdminCommand);
-        } catch (DataIntegrityViolationException e) {
-            logger.info("계정이 이미 존재합니다");
-        }
-        try {
             userService.createUser(createUserCommand);
         } catch (DataIntegrityViolationException e) {
             logger.info("계정이 이미 존재합니다");
         }
+    }
 
+    private void createAdmin() {
+        CreateUserCommand createAdminCommand = new CreateUserCommand(
+                "admin",
+                "12341234",
+                "관리자",
+                "ga.miro3721",
+                "gmail.com",
+                "010",
+                "1234",
+                "5678"
+        );
+        try {
+            userService.createAdmin(createAdminCommand);
+        } catch (DataIntegrityViolationException e) {
+            logger.info("계정이 이미 존재합니다");
+        }
     }
 
 }
--- src/main/java/com/kalgooksoo/cms/config/SecurityConfig.java
+++ src/main/java/com/kalgooksoo/cms/config/SecurityConfig.java
@@ -12,11 +12,18 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AuthorizeHttpRequestsConfigurer;
+import org.springframework.security.config.annotation.web.configurers.RememberMeConfigurer;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.RememberMeServices;
+import org.springframework.security.web.authentication.rememberme.InMemoryTokenRepositoryImpl;
+import org.springframework.security.web.authentication.rememberme.PersistentTokenBasedRememberMeServices;
+import org.springframework.security.web.authentication.rememberme.PersistentTokenRepository;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+import java.util.UUID;
 
 @Configuration
 @EnableWebSecurity
@@ -37,6 +44,11 @@ public class SecurityConfig {
     }
 
     @Bean
+    public PersistentTokenRepository persistentTokenRepository() {
+        return new InMemoryTokenRepositoryImpl();
+    }
+
+    @Bean
     public AuthenticationManager authenticationManager() {
         DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
         authenticationProvider.setUserDetailsService(userDetailsService());
@@ -51,7 +63,19 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests(this::handleAuthorizeHttpRequests);
+        http.rememberMe(this::handleRememberMe);
         return http.build();
+    }
+
+    @Bean
+    public RememberMeServices rememberMeServices() {
+        PersistentTokenBasedRememberMeServices services = new PersistentTokenBasedRememberMeServices(
+                UUID.randomUUID().toString(),
+                userDetailsService(),
+                persistentTokenRepository()
+        );
+        services.setAlwaysRemember(false);
+        return services;
     }
 
     private void handleAuthorizeHttpRequests(AuthorizeHttpRequestsConfigurer<HttpSecurity>.AuthorizationManagerRequestMatcherRegistry authorizationManagerRequestMatcherRegistry) {
@@ -60,6 +84,17 @@ public class SecurityConfig {
                 .requestMatchers(new AntPathRequestMatcher("/admins/**")).hasRole("ADMIN")
                 .anyRequest()
                 .permitAll();
+    }
+
+    private void handleRememberMe(RememberMeConfigurer<HttpSecurity> httpSecurityRememberMeConfigurer) {
+        String key = UUID.randomUUID().toString();
+        int tokenValiditySeconds = 60 * 60 * 24;
+        httpSecurityRememberMeConfigurer
+                .key(key)
+                .rememberMeParameter("rememberMe")
+                .userDetailsService(userDetailsService())
+                .tokenRepository(persistentTokenRepository())
+                .tokenValiditySeconds(tokenValiditySeconds);
     }
 
 }
