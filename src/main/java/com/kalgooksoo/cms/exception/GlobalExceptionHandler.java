a8ba6090be4cfb87169cfb9f5a2172810f7bbfe5
kimdoyeob <ga.miro3721@gmail.com>
2024-06-07 16:23:36 +0900

    feat: GlobalExceptionHandler.handleAuthenticationException 추가

--- src/main/java/com/kalgooksoo/cms/exception/GlobalExceptionHandler.java
+++ src/main/java/com/kalgooksoo/cms/exception/GlobalExceptionHandler.java
@@ -4,6 +4,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -29,8 +30,24 @@ public class GlobalExceptionHandler {
         return "error/404";
     }
 
+    /**
+     * 자원에 대한 접근 권한이 없는 경우
+     * @param e AccessDeniedException
+     * @return 계정 인증 화면
+     */
     @ExceptionHandler(AccessDeniedException.class)
     public String handleAccessDeniedException(AccessDeniedException e) {
+        logger.error(e.getMessage());
+        return "redirect:/sign-in";
+    }
+
+    /**
+     * 인증되지 않은 사용자가 접근한 경우
+     * @param e AuthenticationException
+     * @return 계정 인증 화면
+     */
+    @ExceptionHandler(AuthenticationException.class)
+    public String handleAuthenticationException(AuthenticationException e) {
         logger.error(e.getMessage());
         return "redirect:/sign-in";
     }
