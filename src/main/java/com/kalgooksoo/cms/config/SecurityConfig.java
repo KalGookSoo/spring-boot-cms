690ce7d7afb907532c9c30c9c5329c5a0855071a
kimdoyeob <ga.miro3721@gmail.com>
2024-08-23 17:55:56 +0900

    revert: SecurityConfig 계정 인증 핸들러 원복

--- src/main/java/com/kalgooksoo/cms/config/SecurityConfig.java
+++ src/main/java/com/kalgooksoo/cms/config/SecurityConfig.java
@@ -3,7 +3,11 @@ package com.kalgooksoo.cms.config;
 import com.kalgooksoo.cms.security.service.JpaPersistentTokenRepository;
 import com.kalgooksoo.cms.user.repository.UserPrincipalRepository;
 import com.kalgooksoo.cms.user.service.DefaultUserDetailsService;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -13,16 +17,21 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AuthorizeHttpRequestsConfigurer;
+import org.springframework.security.config.annotation.web.configurers.FormLoginConfigurer;
+import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
 import org.springframework.security.config.annotation.web.configurers.RememberMeConfigurer;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.RememberMeServices;
+import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler;
 import org.springframework.security.web.authentication.rememberme.PersistentTokenBasedRememberMeServices;
 import org.springframework.security.web.authentication.rememberme.PersistentTokenRepository;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
+import java.io.IOException;
 import java.util.UUID;
 
 @Configuration
@@ -30,6 +39,16 @@ import java.util.UUID;
 @EnableMethodSecurity(securedEnabled = true, jsr250Enabled = true)
 @RequiredArgsConstructor
 public class SecurityConfig {
+
+    private final Logger logger = LoggerFactory.getLogger(getClass());
+
+    private static final String LOGIN_PAGE_PATH = "/sign-in";
+
+    private static final String USERNAME_PARAM = "username";
+
+    private static final String PASSWORD_PARAM = "password";
+
+    private static final String LOGOUT_PATH = "/sign-out";
 
     private final UserPrincipalRepository userPrincipalRepository;
 
@@ -65,6 +84,9 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests(this::handleAuthorizeHttpRequests);
+        http.formLogin(this::handleFormLogin);
+        http.userDetailsService(userDetailsService());
+        http.logout(this::configureLogout);
         http.rememberMe(this::handleRememberMe);
         return http.build();
     }
@@ -86,6 +108,25 @@ public class SecurityConfig {
                 .requestMatchers(new AntPathRequestMatcher("/managers/**")).hasAnyRole("MANAGER", "ADMIN")
                 .requestMatchers(new AntPathRequestMatcher("/admins/**")).hasRole("ADMIN")
                 .anyRequest()
+                .permitAll();
+    }
+    private void handleFormLogin(FormLoginConfigurer<HttpSecurity> httpSecurityFormLoginConfigurer) {
+        httpSecurityFormLoginConfigurer
+                .loginPage(LOGIN_PAGE_PATH)
+                .usernameParameter(USERNAME_PARAM)
+                .passwordParameter(PASSWORD_PARAM)
+                .successHandler(new SavedRequestAwareAuthenticationSuccessHandler())
+                .failureHandler(this::handleBadCredentialsException);
+    }
+
+    private void handleBadCredentialsException(HttpServletRequest request, HttpServletResponse response, AuthenticationException exception) throws IOException {
+        logger.error(exception.getMessage());
+        response.sendRedirect(LOGIN_PAGE_PATH);
+    }
+
+    private void configureLogout(LogoutConfigurer<HttpSecurity> httpSecurityLogoutConfigurer) {
+        httpSecurityLogoutConfigurer
+                .logoutUrl(LOGOUT_PATH)
                 .permitAll();
     }
 
