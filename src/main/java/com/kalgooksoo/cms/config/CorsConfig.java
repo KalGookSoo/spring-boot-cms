53ae202e3461a2e7a7d3413e67d9c74d23f9a36f
kimdoyeob <ga.miro3721@gmail.com>
2024-08-28 15:36:12 +0900

    feat: CorsConfig 추가

---
+++ src/main/java/com/kalgooksoo/cms/config/CorsConfig.java
@@ -0,0 +1,26 @@
+package com.kalgooksoo.cms.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+@Configuration
+public class CorsConfig {
+
+    @Profile({"default", "test"})
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+//        config.setAllowCredentials(true);
+        config.addAllowedOrigin("*");
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
+    }
+
+}
