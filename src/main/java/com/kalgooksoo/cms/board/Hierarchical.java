5fe2d3500cf3d70b940bcec3c99df89317b6885f
kimdoyeob <ga.miro3721@gmail.com>
2024-10-16 10:17:25 +0900

    refactor: 계층형 모델 빌드 메서드 추가

--- src/main/java/com/kalgooksoo/cms/board/Hierarchical.java
+++ src/main/java/com/kalgooksoo/cms/board/Hierarchical.java
@@ -3,6 +3,7 @@ package com.kalgooksoo.cms.board;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 public interface Hierarchical<T extends Hierarchical<T>> {
 
@@ -12,6 +13,8 @@ public interface Hierarchical<T extends Hierarchical<T>> {
 
     void setChildren(List<T> children);
 
+    void moveTo(T parent);
+
     default boolean isRoot() {
         return getParent() == null;
     }
@@ -20,8 +23,6 @@ public interface Hierarchical<T extends Hierarchical<T>> {
         return !isRoot();
     }
 
-    void moveTo(T parent);
-
     default T mapChildren(T parentNode, Map<T, List<T>> nodesGroupByParent) {
         List<T> children = nodesGroupByParent.getOrDefault(parentNode, new ArrayList<>());
         List<T> nestedChildren = children.stream()
@@ -29,6 +30,17 @@ public interface Hierarchical<T extends Hierarchical<T>> {
                 .toList();
         parentNode.setChildren(nestedChildren);
         return parentNode;
+    }
+
+    static <T extends Hierarchical<T>> List<Hierarchical<T>> build(List<T> elements) {
+        Map<T, List<T>> collect = elements.stream()
+                .filter(T::hasParent)
+                .collect(Collectors.groupingBy(T::getParent));
+
+        return elements.stream()
+                .filter(T::isRoot)
+                .map(element -> element.mapChildren(element, collect))
+                .collect(Collectors.toList());
     }
 
 }
