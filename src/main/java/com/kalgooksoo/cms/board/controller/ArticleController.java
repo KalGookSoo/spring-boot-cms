c60b167fc7c48da337c04925588f834a60117a4d
kimdoyeob <ga.miro3721@gmail.com>
2024-12-16 15:18:07 +0900

    refactor: 게시글 모듈 화면 레이아웃 적용

--- src/main/java/com/kalgooksoo/cms/board/controller/ArticleController.java
+++ src/main/java/com/kalgooksoo/cms/board/controller/ArticleController.java
@@ -3,17 +3,34 @@ package com.kalgooksoo.cms.board.controller;
 import com.kalgooksoo.cms.board.command.CreateArticleCommand;
 import com.kalgooksoo.cms.board.command.UpdateArticleCommand;
 import com.kalgooksoo.cms.board.entity.Article;
+import com.kalgooksoo.cms.board.entity.Attachment;
 import com.kalgooksoo.cms.board.search.ArticleSearch;
 import com.kalgooksoo.cms.board.service.ArticleService;
 import com.kalgooksoo.cms.message.CmsMessageSource;
+import com.kalgooksoo.core.file.FileIOService;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.FileCopyUtils;
+import org.springframework.util.StreamUtils;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
+import java.util.NoSuchElementException;
+import java.util.Set;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
 
 @Tag(name = "ArticleController", description = "게시글 컨트롤러")
 @Controller
@@ -109,8 +126,66 @@ public class ArticleController {
         return "redirect:/articles/list?categoryId=" + categoryId;
     }
 
+    @GetMapping("/{id}/attachments/{attachmentId}/download")
+    public void getAttachments(
+            @PathVariable String id,
+            @PathVariable String attachmentId,
+            @RequestHeader(HttpHeaders.USER_AGENT) String userAgent,
+            HttpServletResponse response
+    ) throws IOException {
+        Article article = articleService.find(id);
+        Attachment attachment = article.getAttachments()
+                .stream()
+                .filter(e -> attachmentId.equals(e.getId()))
+                .findFirst()
+                .orElseThrow(NoSuchElementException::new);
+        String fileName = URLEncoder.encode(attachment.getOriginalName(), StandardCharsets.UTF_8);
+        ByteArrayInputStream inputStream = FileIOService.read(attachment.getAbsolutePath());
 
+        response.setStatus(HttpServletResponse.SC_OK);
+        response.setContentType(MediaType.APPLICATION_OCTET_STREAM_VALUE);
+        response.setHeader(HttpHeaders.CONTENT_DISPOSITION, getContentDisposition(userAgent, fileName));
+        OutputStream outputStream = response.getOutputStream();
+        FileCopyUtils.copy(inputStream, outputStream);
+    }
 
+    @GetMapping("/{id}/attachments/download-zip")
+    public void getAttachments(
+            @PathVariable String id,
+            @RequestHeader(HttpHeaders.USER_AGENT) String userAgent,
+            HttpServletResponse response
+    ) throws IOException {
+        Article article = articleService.find(id);
+        Set<Attachment> attachments = article.getAttachments();
+        if (attachments.isEmpty()) {
+            throw new NoSuchElementException();
+        }
+        String fileName = URLEncoder.encode(article.getTitle(), StandardCharsets.UTF_8);
 
+        response.setStatus(HttpServletResponse.SC_OK);
+        response.setContentType("application/zip");
+        response.setHeader(HttpHeaders.CONTENT_DISPOSITION, getContentDisposition(userAgent, fileName) + ".zip");
+
+        try (ZipOutputStream outputStream = new ZipOutputStream(response.getOutputStream())) {
+            for (Attachment attachment : attachments) {
+                ZipEntry entry = new ZipEntry(attachment.getOriginalName());
+                outputStream.putNextEntry(entry);
+                ByteArrayInputStream inputStream = FileIOService.read(attachment.getAbsolutePath());
+                StreamUtils.copy(inputStream, outputStream);
+                outputStream.closeEntry();
+            }
+        }
+
+    }
+
+    private String getContentDisposition(String userAgent, String fileName) {
+        String disposition = "attachment;filename=";
+        if (userAgent.contains("MSIE")) {
+            int i = userAgent.indexOf('M', 2);
+            String IEV = userAgent.substring(i + 5, i + 8);
+            disposition = IEV.equalsIgnoreCase("5.5") ? "filename=" : disposition;
+        }
+        return disposition + fileName;
+    }
 
 }
