a709c2e0c770e4288664194a782cc49b83ee70e3
kimdoyeob <ga.miro3721@gmail.com>
2024-12-19 16:34:34 +0900

    feat: 트리 UI collapse, expand 기능 추출

--- src/main/resources/static/js/global.js
+++ src/main/resources/static/js/global.js
@@ -1,23 +1,45 @@
-const handleDateInput = e => {
-    const types = ['datetime-local', 'date', 'time'];
-    if (!types.includes(e.target.type)) {
-        return;
-    }
+document.addEventListener('DOMContentLoaded', e => {
+    const _document = e.target;
+    _document.addEventListener('input', e => {
+        handleDateInput(e);
+    });
+    const handleDateInput = e => {
+        const types = ['datetime-local', 'date', 'time'];
+        if (!types.includes(e.target.type)) {
+            return;
+        }
 
-    const {minRef, maxRef} = e.target.dataset;
-    const $start = minRef && document.getElementById(minRef);
-    if ($start) {
-        $start.max = e.target.value;
-    }
-    const $end = maxRef && document.getElementById(maxRef);
-    if ($end) {
-        $end.min = e.target.value;
-    }
+        const {minRef, maxRef} = e.target.dataset;
+        const $start = minRef && document.getElementById(minRef);
+        if ($start) {
+            $start.max = e.target.value;
+        }
+        const $end = maxRef && document.getElementById(maxRef);
+        if ($end) {
+            $end.min = e.target.value;
+        }
 
-    if (!e.target.validity.valid) {
-        e.target.reportValidity();
-    }
-};
-document.addEventListener('input', e => {
-    handleDateInput(e);
+        if (!e.target.validity.valid) {
+            e.target.reportValidity();
+        }
+    };
+
+    _document.addEventListener('click', e => {
+        handleDetails(e);
+    });
+    const handleDetails = e => {
+        const $expandButton = e.target.closest(`button[name="expand"]`);
+        if ($expandButton) {
+            const $target = document.getElementById($expandButton.dataset.targetRef);
+            const $details = $target.querySelectorAll('details');
+            [...$details].forEach(detail => detail.open = true);
+        }
+
+        const $collapseButton = e.target.closest(`button[name="collapse"]`);
+        if ($collapseButton) {
+            const $target = document.getElementById($collapseButton.dataset.targetRef);
+            const $details = $target.querySelectorAll('details');
+            [...$details].forEach(detail => detail.open = false);
+        }
+    };
 });
