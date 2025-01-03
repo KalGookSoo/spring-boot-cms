b7fd33b481614f00253c85d900ca5bdc7ea9e1c4
kimdoyeob <ga.miro3721@gmail.com>
2025-01-03 15:18:08 +0900

    feat: 서블릿 컨테이너 호환성을 위해 WAR 배포 활성화

--- src/main/java/com/kalgooksoo/cms/CmsApplication.java
+++ src/main/java/com/kalgooksoo/cms/CmsApplication.java
@@ -2,12 +2,14 @@ package com.kalgooksoo.cms;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 /**
  * CMS 애플리케이션 메인 클래스
  */
 @SpringBootApplication
-public class CmsApplication {
+public class CmsApplication extends SpringBootServletInitializer {
 
 	/**
 	 * Spring application context를 생성하고 초기화합니다.
@@ -15,6 +17,19 @@ public class CmsApplication {
 	 */
 	public static void main(String[] args) {
 		SpringApplication.run(CmsApplication.class, args);
+	}
+
+	/**
+	 * Configures the application when deployed in a servlet container.
+	 * This method is called to customize the SpringApplicationBuilder
+	 * during application initialization.
+	 *
+	 * @param application the builder for the application
+	 * @return the configured SpringApplicationBuilder instance
+	 */
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(CmsApplication.class);
 	}
 
 }
