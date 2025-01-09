dddc16ee0d0515eb62e941f4d26835a0704c5181
kimdoyeob <ga.miro3721@gmail.com>
2025-01-09 10:50:05 +0900

    fix: 미사용 코드 제거

--- src/main/java/com/kalgooksoo/cms/repository/DefaultUserRepository.java
+++ src/main/java/com/kalgooksoo/cms/repository/DefaultUserRepository.java
@@ -8,7 +8,6 @@ import jakarta.persistence.TypedQuery;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.lang.NonNull;
 import org.springframework.stereotype.Repository;
