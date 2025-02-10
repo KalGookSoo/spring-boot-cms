afb263cb32d7775306be62ef5fe415c4f87c6d85
kimdoyeob <ga.miro3721@gmail.com>
2025-02-11 05:11:05 +0900

    refactor: API 위치 이동

--- src/main/java/com/kalgooksoo/cms/MainController.java
+++ src/main/java/com/kalgooksoo/cms/MainController.java
@@ -1,16 +1,7 @@
 package com.kalgooksoo.cms;
 
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpSession;
-import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-
-import java.util.Enumeration;
-import java.util.HashMap;
-import java.util.Map;
 
 /**
  * 메인 컨트롤러
@@ -26,31 +17,6 @@ public class MainController {
     @GetMapping("/")
     public String index() {
         return "redirect:/swagger-ui/index.html";
-    }
-
-    /**
-     * 세션 식별자를 반환합니다.
-     *
-     * @param request 요청
-     * @return 세션 식별자
-     */
-    @PreAuthorize("hasRole('ADMIN')")
-    @ResponseBody
-    @GetMapping("/session")
-    public ResponseEntity<Map<String, Object>> getSession(HttpServletRequest request) {
-        HttpSession session = request.getSession();
-        Map<String, Object> responseBody = new HashMap<>();
-        responseBody.put("id", session.getId());
-
-        Map<String, Object> attributes = new HashMap<>();
-        Enumeration<String> attributeNames = session.getAttributeNames();
-        while (attributeNames.hasMoreElements()) {
-            String attributeName = attributeNames.nextElement();
-            attributes.put(attributeName, session.getAttribute(attributeName));
-        }
-        responseBody.put("attributes", attributes);
-
-        return ResponseEntity.ok(responseBody);
     }
 
 }
--- src/main/java/com/kalgooksoo/cms/controller/SessionApiController.java
+++ src/main/java/com/kalgooksoo/cms/controller/SessionApiController.java
@@ -1,17 +1,45 @@
 package com.kalgooksoo.cms.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Enumeration;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/session")
 public class SessionApiController {
 
+    @PreAuthorize("hasRole('ADMIN')")
+    @GetMapping
+    public ResponseEntity<Map<String, Object>> getSession(HttpServletRequest request) {
+        HttpSession session = request.getSession();
+        Map<String, Object> responseBody = new HashMap<>();
+        responseBody.put("id", session.getId());
+
+        Map<String, Object> attributes = new HashMap<>();
+        Enumeration<String> attributeNames = session.getAttributeNames();
+        while (attributeNames.hasMoreElements()) {
+            String attributeName = attributeNames.nextElement();
+            attributes.put(attributeName, session.getAttribute(attributeName));
+        }
+        responseBody.put("attributes", attributes);
+
+        return ResponseEntity.ok(responseBody);
+    }
+
     @GetMapping("/refresh")
-    ResponseEntity<?> refresh() {
-        return ResponseEntity.ok().body(null);
+    public ResponseEntity<Void> refresh(HttpServletRequest request) {
+        HttpSession session = request.getSession();
+        int maxInactiveInterval = session.getMaxInactiveInterval();
+        session.setMaxInactiveInterval(maxInactiveInterval);
+        return ResponseEntity.ok().build();
     }
 
 }
